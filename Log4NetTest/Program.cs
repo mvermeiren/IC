f02221f85e79fe1114be7623db112bd85c8877cc
unknown <mvermeiren@L-2CE3241QMB.axxes.lan>
2015-09-01 17:01:10 +0200

    commit

---
+++ Log4NetTest/Program.cs
@@ -0,0 +1,32 @@
+﻿using log4net;
+using log4net.Config;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Log4NetTest
+{
+    class Program
+    {
+
+        private static readonly ILog log = LogManager.GetLogger(typeof(Program));
+
+        static void Main(string[] args)
+        {
+            //BasicConfigurator.Configure();
+            XmlConfigurator.Configure();
+
+
+            log.Info("pre-start2");
+
+            Console.WriteLine("Log4Net Test!");
+
+            log.Error("error test2");
+            log.Debug("error evaded, debug succes2");
+            Console.ReadLine();
+
+        }
+    }
+}
